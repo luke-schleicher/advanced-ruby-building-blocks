2188cff7c330d042d674940eca41e55ff8c69f7d
schleiwalker17 <luke.j.schleicher@gmail.com>
2016-07-23 15:15:52 -0500

    bubble_sort_by.rb first draft

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,7 +1,24 @@
-def bubble_sort_by(items) do |left,right|
-  left.length - right.length
+def bubble_sort_by(array)
+	begin
+		times_swapped = 0
+		array.each_index do |i|
+			if i == array.length - 1
+				break
+			else
+				value = yield array[i+1], array[i]
+				if value < 0
+					array[i], array [i+1] = array[i+1], array[i]
+					times_swapped += 1
+				end
+			end
+		end
+		
+	end while times_swapped > 0
+	puts array
 end
 
 greetings = ["hi","hello","hey"]
 
-bubble_sort_by(greetings)
+bubble_sort_by(greetings) do |second, first|
+	second.length - first.length
+end
